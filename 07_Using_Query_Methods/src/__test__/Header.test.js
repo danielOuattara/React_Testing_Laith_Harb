0f911d47823bed7491119c4630955ea93fb8a691
daniel ouattara <daniel.ouattara@gmx.com>
2021-11-08 12:09:17 +0100

    corrections 07

--- 07_Using_Query_Methods/src/__test__/Header.test.js
+++ 07_Using_Query_Methods/src/__test__/Header.test.js
@@ -2,69 +2,84 @@ import { render, screen } from '@testing-library/react';
 import Header from './../components/Header/Header';
 
 
-// test('should render the same text passed in title prop', () => {
-//   render(<Header title="My header"/>);
-//   const headingElement = screen.getByText(/my header/i);
-//   expect(headingElement).toBeInTheDocument();
-// });
+test('should render the same text passed in title prop', () => {
+  render(<Header title="My header"/>);
+  const headingElement = screen.getByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
 
-// it('should again render the same text passed in title prop', () => {
-//   render(<Header title="My header"/>);
-//   const headingElement = screen.getByText(/my header/i);
-//   expect(headingElement).toBeInTheDocument();
-// });
+it('should again render the same text passed in title prop', () => {
+  render(<Header title="Cats"/>);
+  const headingElement = screen.getByText(/cats/i);
+  expect(headingElement).toBeInTheDocument();
+});
 
 // it('should get the role of header', () => {
-//   render(<Header title="My header"/>);
-//   const headingElement = screen.getByRole("heading");
-//   expect(headingElement).toBeInTheDocument();
-// });
-
-// it('should render the same text passed in title prop', () => {
-//   render(<Header title="My header"/>);
-//   const headingElement = screen.getByRole("heading");
-//   expect(headingElement).toBeInTheDocument();
-// });
-
-// it('should get by its role', () => {
-//   render(<Header title="My header"/>);
-//   const headingElement = screen.getByRole("heading", {name: "React Testing Library"});
-//   expect(headingElement).toBeInTheDocument();
-// });
-
-// it('should get by its title', () => {
-//   render(<Header title="My header"/>);
-//   const headingElement = screen.getByTitle("header");
-//   expect(headingElement).toBeInTheDocument();
-// });
-
-// it('should get by its id', () => {
-//   render(<Header title="My header"/>);
-//   const headingElement = screen.getByTestId("header-1");
-//   expect(headingElement).toBeInTheDocument();
-// });
-
-
-// // FIND BY 
-
-
-// test('should findBy the same text passed in title prop', async () => {
-//   render(<Header title="My header"/>);
-//   const headingElement =  await screen.findByText(/my header/i);
-//   expect(headingElement).toBeInTheDocument();
-// });
-
-
-// // QUERY BY
-
-
-// test('should output "birds" not to be in the app',() => {
 //   render(<Header/>);
-//   const headingElement =  screen.queryByText(/birds/i);
-//   expect(headingElement).not.toBeInTheDocument();
+//   const headingElement = screen.getByRole("heading");
+//   expect(headingElement).toBeInTheDocument();
 // });
 
-// // GET ALL BY
+
+//-----------------------------------------------------
+/* 
+This test failed, because its method (getByRole) catches 2 items. 
+ getByRoles should only catch one item
+ see: React_Testing_Laith_Harb\05_Introduction_Query_Methods\tut_05_01.png 
+*/
+
+// it('should render again the same text passed in title prop', () => {
+//   render(<Header title="My header"/>);
+//   const headingElement = screen.getByRole("heading");
+//   expect(headingElement).toBeInTheDocument();
+// });
+
+//----------------------------------------------------------------------
+
+it('should get by its role and name', () => {
+  render(<Header title="My header"/>);
+  const headingElement = screen.getByRole("heading", {name: "React Testing Library"});
+  expect(headingElement).toBeInTheDocument();
+});
+
+it('should get by its title', () => {
+  render(<Header title="My header"/>);
+  const headingElement = screen.getByTitle("header");
+  expect(headingElement).toBeInTheDocument();
+});
+
+it('should get by its id', () => {
+  render(<Header title="My header"/>);
+  const headingElement = screen.getByTestId("header-1");
+  expect(headingElement).toBeInTheDocument();
+});
+
+
+/* findBy...() 
+=============*/
+
+
+/* findBy() must be Async/await*/
+test('should findBy the same text passed in title prop', async () => {
+  render(<Header title="My header"/>);
+  const headingElement =  await screen.findByText(/my header/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+
+/* queryBy...()
+================*/
+
+
+test('should output "birds" not to be in the app',() => {
+  render(<Header/>);
+  const headingElement =  screen.queryByText(/birds/i);
+  expect(headingElement).not.toBeInTheDocument();
+});
+
+
+/* getAllBy...()
+================*/
 
 
 test('should get 2 headers in the app using getAllByRole',() => {
--- 07_Using_Query_Methods/src/components/Header/Header.js
+++ 07_Using_Query_Methods/src/components/Header/Header.js
@@ -13,7 +13,7 @@ export default function Header({title}) {
     return (
         <>
             <h1 className="header" data-testid="header-1">React Testing Library </h1>
-            <h1 title="header" className="header">{title}</h1>
+            <h3 title="header" className="header">{title}</h3>
         </>
     ) 
 }
